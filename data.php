880933b17d126366a5c74fefce4778aa59000245
Mirosław Sulowski <mir.sulowski@gmail.com>
2022-11-07 16:07:14 +0100

    Bootstrap cd

--- data.php
+++ data.php
@@ -2,44 +2,94 @@
 
 function Json($content = null, $header = 'Content-type: application/json')
 {
-    header($header);
+//    header($header);
+
     if (!empty($content)) {
-        echo $content;
+        echo json_encode($content, JSON_PRETTY_PRINT);
     }
     else {
-        echo '';
+        json_encode('', JSON_PRETTY_PRINT);
     }
     die();
 }
 
 function getData($search = null)
 {
-    $file = 'data.txt';
-    $data = [
-        [
-            'name' => 'Niedrzwica duża',
-            'btn' => 'buttons set',
-        ],
-        [
-            'name' => 'Tarnobrzeg',
-            'btn' => 'buttons set',
-        ],
-        [
-            'name' => 'Radomyśl',
-            'btn' => 'buttons set',
-        ],
-    ];
+    $file = __DIR__ . DIRECTORY_SEPARATOR
+        . DIRECTORY_SEPARATOR . 'data'
+        . DIRECTORY_SEPARATOR . 'proxy_list.txt';
+    $fileData = file_get_contents($file);
+    $fileData = preg_split('/[\n\r]+/', $fileData);
+    $fileDataRow = [];
+    $search = strtoupper($search);
 
+    foreach ($fileData as $row) {
+        if (@strstr($row, $search)) {
+            $rowExplode = preg_split('/[\s]+/', $row);
 
-    return $data;
+            foreach ($rowExplode as $item) {
+                if (@strstr($item, $search)) {
+                    $itemNameExplode = explode('_', $item);
+
+                    foreach ($itemNameExplode as $itemNameExplodeItem) {
+                        if (@strstr($itemNameExplodeItem, $search)) {
+                            $fileDataRowName = $itemNameExplodeItem;
+                            break;
+                        }
+                    }
+                    $fileDataRow [] = $fileDataRowName;
+                    break;
+                }
+            }
+        }
+    }
+    $return = array_unique($fileDataRow);
+    sort($return, SORT_NATURAL);
+
+    return $return;
 }
 
-if (isset($_GET['string'])) {
+function getFullData($search = null)
+{
+    $file = __DIR__ . DIRECTORY_SEPARATOR
+        . DIRECTORY_SEPARATOR . 'data'
+        . DIRECTORY_SEPARATOR . 'proxy_list.txt';
+    $fileData = file_get_contents($file);
+    $fileData = preg_split('/[\n\r]+/', $fileData);
+    $fileDataRow = [];
+    $fileDataRowItems = [];
 
-    if ($_GET['string'] == 'search') {
+    foreach ($fileData as $row) {
 
-        $s = filter_var($_GET['s'], FILTER_SANITIZE_STRING);
-        $data = getData($s);
+        if (@strstr($row, $search)) {
+            $rowExplode = preg_split('/[\s]+/', $row);
+            $fileDataRowItems = [];
+
+            foreach ($rowExplode as $item) {
+                $fileDataRowItems [] = $item;
+            }
+            $fileDataRow [] = $fileDataRowItems;
+        }
+    }
+
+//    sort($fileDataRow, SORT_NATURAL);
+
+    return $fileDataRow;
+}
+
+if (isset($_GET['action'])) {
+
+    if ($_GET['action'] == 'search') {
+
+        $keyword = filter_input(INPUT_POST, 'keyword', FILTER_SANITIZE_SPECIAL_CHARS);
+        $data = getData($keyword);
+        Json($data);
+    }
+
+    if ($_GET['action'] == 'search_callback') {
+
+        $keyword = filter_input(INPUT_POST, 'keyword', FILTER_SANITIZE_SPECIAL_CHARS);
+        $data = getFullData($keyword);
         Json($data);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,43 +1,167 @@
 $(document).ready(function () {
+
+    var searchAjax = function (textValue) {
+        // console.log(textValue);
+
+        $.ajax({
+            dataType: "json",
+            cache: false,
+            type: "POST",
+            url: "data.php?action=search_callback",
+            data: {
+                'keyword': textValue
+            },
+            beforeSend: function (xhr) {
+                $('#searchInputIcon').append('<div id="loader" class="spinner-border" role="status">\n' +
+                    '  <span class="sr-only">...</span>\n' +
+                    '</div>');
+            },
+            success: function (data) {
+                $('#loader').remove();
+                $('#multiCollapseOriginalList').html(data);
+                console.log(data);
+                let countRows = 0;
+                countRows = data.length;
+
+                if (countRows) {
+                    let rowsContainer = $('#resRowsList');
+                    let rowsContainerHtml = '';
+
+                    for (let i = 0; i < countRows; i++) {
+                        rowsContainerHtml += '<div style="cursor:pointer;" id="search_full_row_' + i + '" class="card">\n' +
+                            '                    <div class="card-body pt-2 pb-2 pl-2 pr-2">\n' +
+                            '                        <h5 class="card-title mb-0">' + data[i] + '</h5>\n' +
+                            '                    </div>\n' +
+                            '                </div>';
+                    }
+
+                    rowsContainer.html(rowsContainerHtml);
+
+                    // $("div[id^='search_row_']").each(function (i) {
+                    //     $(this).on('click', function (event) {
+                    //         event.preventDefault();
+                    //         thisInput.val($(this).find('h5').text());
+                    //         searchAjax(thisInput);
+                    //     });
+                    // });
+                } else {
+
+                }
+            },
+            error: function () {
+                $('#loader').remove();
+            }
+        });
+    };
+
     var searchBarInputText = function () {
-        $('#orgSearchNameId').off('keyup').on('keyup', debounce(function (event) {
+        $('#searchInput').off('keyup').on('keyup', debounce(function (event) {
             // do the Ajax request
+            var thisInput = $(this);
             const inputNameOriginalValue = $(this).val();
-            const bodyAll = $('#bodyAll');
-            const criteria = {
-                'type': bodyAll.data('sBarType'),
-                'order': bodyAll.data('sBarSort')
-            };
-            // console.log('Was send with criteria ' + criteria.type + +criteria.order);
 
             $.ajax({
+                dataType: "json",
+                cache: false,
                 type: "POST",
-                url: "?search=original",
+                url: "data.php?action=search",
                 data: {
-                    'keyword': inputNameOriginalValue,
-                    'type': criteria.type,
-                    'order': criteria.order
+                    'keyword': inputNameOriginalValue
                 },
                 beforeSend: function (xhr) {
-                    $('#multiCollapseOriginalList').html('<div class="spinner-border" role="status">\n' +
+                    $('#iconSearchSpace').append('<div id="loader" class="spinner-border" role="status">\n' +
                         '  <span class="sr-only">Loading...</span>\n' +
                         '</div>');
                 },
                 success: function (data) {
+                    $('#loader').remove();
                     $('#multiCollapseOriginalList').html(data);
+                    console.log(data);
+                    let countRows = 0;
+                    countRows = data.length;
 
-                    $("a[id^='remove_original_']").each(function (i) {
-                        $(this).on('click', function (event) {
-                            event.preventDefault();
-                            // if (confirm("Napewno usunąć") == true) {
-                            // importMixer('?remove_original=', '#remove_original_', $(this).attr('id').split('_').pop());
-                            // }
+                    if (countRows) {
+                        let rowsContainer = $('#resRowsList');
+
+                        let refreshButton = $('#searchTextRefresh').find('i').first();
+
+                        if (refreshButton.hasClass('fa-chevron-circle-down')) {
+                            refreshButton.removeClass('text-danger').addClass('text-success');
+                        }
+
+                        if (refreshButton.hasClass('fa-chevron-circle-up')) {
+                            refreshButton.removeClass('fa-chevron-circle-up').addClass('fa-chevron-circle-down');
+                        }
+                        // else {
+                        //     refreshButton.removeClass('fa-chevron-circle-up').addClass('fa-chevron-circle-down');
+                        //     refreshButton.removeClass('text-danger').addClass('text-success');
+                        // }
+
+                        if (!rowsContainer.hasClass('searchResultShow')) {
+                            rowsContainer.removeClass('searchResultHide').addClass('searchResultShow');
+                        } else {
+                            rowsContainer.removeClass('searchResultShow').addClass('searchResultHide');
+                        }
+
+                        let rowsContainerHtml = '';
+
+                        for (let i = 0; i < countRows; i++) {
+                            rowsContainerHtml += '<div style="cursor:pointer;" id="search_row_' + i + '" class="card">\n' +
+                                '                    <div class="card-body pt-2 pb-2 pl-2 pr-2">\n' +
+                                '                        <h5 class="card-title mb-0">' + data[i] + '</h5>\n' +
+                                '                    </div>\n' +
+                                '                </div>';
+                        }
+
+                        rowsContainer.html(rowsContainerHtml);
+
+                        $("div[id^='search_row_']").each(function (i) {
+                            $(this).on('click', function (event) {
+                                event.preventDefault();
+                                let textSelect = $(this).find('h5').text();
+                                thisInput.val(textSelect);
+                                searchAjax(textSelect);
+                            });
                         });
-                    });
+                    } else {
+
+                    }
+                },
+                error: function () {
+                    $('#loader').remove();
                 }
             });
         }, 500));
     };
+    searchBarInputText();
+
+    const clickRefresh = function () {
+        $('#searchTextRefresh').on('click', function (event) {
+            event.preventDefault();
+
+            let searchResult = $('#resRowsList');
+            let refreshButton = $(this).find('i').first();
+            // console.log(refreshButton);
+            // console.log(refreshButton.attr('class'));
+
+            if (refreshButton.hasClass('fa-chevron-circle-down')) {
+                refreshButton.removeClass('fa-chevron-circle-down').addClass('fa-chevron-circle-up');
+                refreshButton.removeClass('text-success').addClass('text-danger');
+            } else {
+                refreshButton.removeClass('fa-chevron-circle-up').addClass('fa-chevron-circle-down');
+                refreshButton.removeClass('text-danger').addClass('text-success');
+            }
+
+            console.log(refreshButton.attr('class'));
+
+            if (!searchResult.hasClass('searchResultShow')) {
+                searchResult.removeClass('searchResultHide').addClass('searchResultShow');
+            } else {
+                searchResult.removeClass('searchResultShow').addClass('searchResultHide');
+            }
+        });
+    };
+    clickRefresh();
 
     function sendRefreshOriginal() {
         const bodyAll = $('#bodyAll');
@@ -86,7 +210,8 @@ $(document).ready(function () {
     }
 
     const showOnPx = 100;
-    const backToTopButton = document.querySelector(".back-to-top")
+    let backToTopButton = null;
+    backToTopButton = document.querySelector(".back-to-top");
 
     const scrollContainer = () => {
         return document.documentElement || document.body;
@@ -100,7 +225,7 @@ $(document).ready(function () {
         }
     });
 
-    if (undefined !== backToTopButton) {
+    if (backToTopButton !== null) {
         backToTopButton.addEventListener("click", function (e) {
             document.body.scrollIntoView({
                 behavior: "smooth",
